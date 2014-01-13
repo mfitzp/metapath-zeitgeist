c1e7783df3795e5a4d4c064cb624c026021a8e63
Martin Fitzpatrick <mfitzp@abl.es>
2014-01-13 23:24:52 +0000

    Update to work with v2.0

--- zeitgeist.py
+++ zeitgeist.py
@@ -12,7 +12,7 @@ from PyQt5.QtPrintSupport import *
 
 from plugins import VisualisationPlugin
 
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 
 import os, re, time
 from copy import copy
@@ -22,41 +22,47 @@ from sklearn.decomposition import PCA
 
 import ui, db, utils, threads
 from data import DataSet, DataDefinition
+from views import D3PrerenderedView
 
 from poster.encode import multipart_encode
 
 import requests
 
 
-class ZeitgeistView( ui.AnalysisView ):
-    def __init__(self, plugin, parent, auto_consume_data=True, **kwargs):
-        super(ZeitgeistView, self).__init__(plugin, parent, **kwargs)
+class ZeitgeistApp( ui.AnalysisApp ):
+    def __init__(self, auto_consume_data=True, **kwargs):
+        super(ZeitgeistApp, self).__init__(**kwargs)
 
         # Define automatic mapping (settings will determine the route; allow manual tweaks later)
         
         self.addDataToolBar()
         self.addFigureToolBar()
         
-        self.data.add_input('input') # Add input slot
+        #self.data.add_input('input') # Add input slot
+        self.views.addView( D3PrerenderedView(self), 'View')
         
-        #self.table = QTableView()
+        #self.table = QTableApp()
         #self.table.setModel(self.data.o['output'].as_table)
         #self.tabs.addTab(self.table,'Table')
         
         self.predefined_geist = {
             'Omics':['metabolomics','genomics','transcriptomics','proteomics','metabonomics'],
             'Arthritis':['RA','SLE','OA','JA','REA'],
-            'Cytokines':['IL1alpha','IL1beta','IL10','IL10R','IL17','IL2','IL3','IL30','IL4','IL5','IL6','TNFalpha'],
+            'Cytokines':['IL1','IL1alpha','IL1beta','IL2','IL3','IL4','IL5','IL6','IL10','IL10R','IL17','IL30','IFNgamma','TNFalpha'],
             'Viruses':['H1N1','H5N1', 'EBV', 'HIV','HPV','HSV','HPV5','HPV8']
         }
+
+        self.config.set_defaults({
+            'predefined_geist':'Omics',
+        })
         
-        self._show_predefined_geist = 'Omics'
         
         t = self.addToolBar('Zeitgeist')
         t.zg_control = QComboBox()
-        t.zg_control.currentIndexChanged.connect(self.onChangeGeist)
         t.zg_control.addItems( [h for h in self.predefined_geist.keys()] )
+        self.config.add_handler('predefined_geist', t.zg_control)
         t.addWidget(t.zg_control)
+        
         self.toolbars['zeitgeist'] = t
 
         
@@ -67,45 +73,27 @@ class ZeitgeistView( ui.AnalysisView ):
             })
         )
 
-        self.data.source_updated.connect( self.onDataChanged ) # Auto-regenerate if the source data is modified
+        self.data.source_updated.connect( self.autogenerate ) # Auto-regenerate if the source data is modified
         if auto_consume_data:
             self.data.consume_any_of( self.m.datasets[::-1] ) # Try consume any dataset; work backwards
+        self.config.updated.connect( self.autogenerate ) # Auto-regenerate if the configuration is changed
 
-    def onChangeGeist(self):
-        self._show_predefined_geist = self.toolbars['zeitgeist'].zg_control.currentText()
-        self.set_name( self._show_predefined_geist )
-        self.generate()
-        
 
-    def onDataChanged(self):
-        self.generate()
-        
     def batches(self, data, batch_size):
         for i in range(0, len(data), batch_size):
                 yield data[i:i+batch_size]    
      
-    def generate(self):
-        self.worker = threads.Worker(self.zeitgeist, dso=self.data.get('input')) #, config=self.config, options=self.options)
-        self.start_worker_thread(self.worker)
-
-    def generated(self, dso, metadata ):
-        self.render(metadata, template='d3/zeitgeist.svg')
-
-
     # Generate Zeitgeist
-    def zeitgeist(self,dso):    
-        
+    def generate(self, input=None):    
+        dso = input
         # Build a list object of class, x, y
         #terms = ['IL1','IL-6','TNF-a','TNF-alpha','IL-10','IL-21','IL-12']
         #terms = ['glucose','lactate','pyruvate','succinate','maleate']
         #terms = ['metabolomics','genomics','transcriptomics','proteomics','metabonomics']
-        if self._show_predefined_geist:
-            terms = self.predefined_geist[ self._show_predefined_geist ]
+        terms = self.predefined_geist[ self.config.get('predefined_geist') ]
                 
-        figure_data = []
+        figure_data = OrderedDict()
         
-        # Request data from the MLTrends site; in batches of 5 (query limit)
-        # http://www.ogic.ca/mltrends/?search_type=titles%20and%20abstracts;norm_type=word%20count;graph_scale=log;query=glucose%20AND%20lactate%2C%20glucose%2C%20lactate;Graph%21=Graph%21&DOWNLOAD=1
         # Pubmed alternative (one at a time; multi-request for each year?)
         # http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&rettype=count&term=maltose%20[tiab]%20and%202011:2013%20[dp]&retmode=xml
         values = {
@@ -116,10 +104,16 @@ class ZeitgeistView( ui.AnalysisView ):
             }
         
         year_range = 5
-        years = range(1970,2012,year_range)
+        years = range(1960,2012,year_range)
+
+        for year in years:
+            figure_data[year] = []
         
-        for term in terms: # Iterate in 5s using a generator (query limit)
+        terms_n = float(len(terms))
+        
+        for n, term in enumerate(terms): # Iterate in 5s using a generator (query limit)
             previous_n = 0
+            self.updateProgress( (n/terms_n)*100 )
             for year in years:
                 values['term'] = "%s [tiab] %d:%d [ppdat]" % (term, year, year+year_range-1)
                 
@@ -139,102 +133,26 @@ class ZeitgeistView( ui.AnalysisView ):
                 # We have an n!
                 n = int(n)        
                 n_delta_rel = (n-previous_n)/float(n) if n >0 else 0
-                figure_data.append( ( year, term, n, n_delta_rel) )  
+                figure_data[year].append( (term, n) )  
                 previous_n = n
                     
-                
         metadata = {
             'figure':{
                 'data':figure_data,
                 },
         }
-        
-        return {'dso':dso, 'metadata':metadata}
-                
+
+        return {'metadata': metadata }       
+
+    def prerender(self, metadata):
+        return {'View':{'metadata':metadata, 'template':'d3/stacked-area.svg'}}                
      
-    # Generate Zeitgeist
-    def zeitgeist_mltrends(self,dso):    
         
-        # Build a list object of class, x, y
-        #terms = ['IL1','IL-6','TNF-a','TNF-alpha','IL-10','IL-21','IL-12']
-        #terms = ['glucose','lactate','pyruvate','succinate','maleate']
-        #terms = ['metabolomics','genomics','transcriptomics','proteomics','metabonomics']
-        if self._show_predefined_geist:
-            terms = self.predefined_geist[ self._show_predefined_geist ]
- 
-                
-        figure_data = []
-        
-        # Request data from the MLTrends site; in batches of 5 (query limit)
-        # http://www.ogic.ca/mltrends/?search_type=titles%20and%20abstracts;norm_type=word%20count;graph_scale=log;query=glucose%20AND%20lactate%2C%20glucose%2C%20lactate;Graph%21=Graph%21&DOWNLOAD=1
-        # Pubmed alternative (one at a time; multi-request for each year?)
-        # http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&rettype=count&term=maltose%20[tiab]%20and%202011:2013%20[dp]&retmode=xml
-        values = {
-            # Raw data download doesn't pay attention to scaling, etc. boo!
-            'search_type':'titles', # and abstracts',
-            'norm_type':'none',
-            'graph_scale':'linear',
-            'Graph!':'Graph!',
-            'DOWNLOAD':'1',
-            }
-        
-        for termset in self.batches( terms, 5): # Iterate in 5s using a generator (query limit)
-            
-            query = values
-            query['query']=','.join( ['"%s"' %t if ' ' in t else t for t in termset] )
-            print query['query']
-            
-            data = urllib.urlencode(values)
-
-            request = urllib2.Request('http://www.ogic.ca/mltrends/', data)
-
-            try:
-                response = urllib2.urlopen(request)
-            except urllib2.HTTPError, e:
-                print e
-                sys.exit()
-
-            except urllib2.URLError, e:
-                print e
-                sys.exit()
-
-            tsv = response.read()
-            #print tsv
-            rows = tsv.split("\n")
-            # The first row gives us our headers
-            headers = [ h.strip('"') for h in rows[0].split("\t")[1:] ]
-            year_cache = dict()
-            last_year = None
-            for row in rows[1:-2]: # Skip latest year (-2 for blank line at end)
-                cols = row.split("\t")
-                year = cols[0]
-                for n, col in enumerate(cols[1:]):
-                    value = int('0'+col)
-                    year_data = ( year, headers[n], value )
-                    year_cache[ (year,headers[n]) ] = value
-
-                    if last_year and value > 0:
-                        last_year_value = year_cache[ (last_year, headers[n]) ]
-                        delta_value = value - last_year_value
-                        figure_data.append( ( year, headers[n], value, delta_value/float(value)) )   # tuple year, term, count )
-                last_year = year
-                
-        metadata = {
-            'figure':{
-                'data':figure_data,
-                },
-        }
-        
-        return {'dso':dso, 'metadata':metadata}
-                
 
 class Zeitgeist(VisualisationPlugin):
 
     def __init__(self, **kwargs):
         super(Zeitgeist, self).__init__(**kwargs)
-        self.register_app_launcher( self.app_launcher )
-
-    def app_launcher(self, **kwargs):
-        return ZeitgeistView( self, self.m, **kwargs )
+        self.register_app_launcher( ZeitgeistApp )
         
         
